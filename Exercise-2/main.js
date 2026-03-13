0725dca82ba2c85090461256faf840821e432a0e
mohamed-awil <mohamedawil110@gmail.com>
2026-03-13 16:40:51 +0300

    exercise-2 submited

---
+++ Exercise-2/main.js
@@ -0,0 +1,30 @@
+// comparison operators assignment
+
+let a = 15;
+let b = "15";
+
+// equal to
+console.log(a == b)
+
+// not equal to
+console.log(a != b)
+
+// strictly equal to
+console.log(a === b)
+
+// strictly not equal to
+console.log(a !== b)
+
+// ------------
+
+// greather than
+console.log(b > a)
+
+// less than
+console.log(b < a)
+
+// greater than or equal to
+console.log(b >= a)
+
+// less than or equal to
+console.log(b <= a)
