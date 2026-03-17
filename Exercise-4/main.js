a2f621c41446179910e2459ce664a1475b753d70
mohamed-awil <mohamedawil110@gmail.com>
2026-03-17 15:00:03 +0300

    exercise-4 submited

---
+++ Exercise-4/main.js
@@ -0,0 +1,24 @@
+
+// function declaration
+
+function add(a,b) {
+    return a+b;
+}
+
+console.log(add(5,5));
+console.log(add(10,15));
+console.log(add(25,25));
+console.log(add(50,50));
+console.log(add(500,500));
+
+// function experession
+
+const addition = function (a,b) {
+    return a+b;
+}
+
+console.log(addition(100,100));
+console.log(addition(200,200));
+console.log(addition(300,300));
+console.log(addition(400,400));
+console.log(addition(500,500));
