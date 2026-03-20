d9d2b9b4e91650e922a23a8f4c4153e97cb23175
mohamed-awil <mohamedawil110@gmail.com>
2026-03-20 15:30:33 +0300

    exercise-7 submited

---
+++ Exercise-7/main.js
@@ -0,0 +1,16 @@
+// Array
+
+let colors =["Red", "Green", "Blue"];
+
+// array methods
+console.log(colors);
+
+colors.pop();
+console.log("After Pop");
+console.log(colors);
+
+colors.unshift("Yellow");
+console.log("After Unshift");
+console.log(colors);
+
+console.log(colors.length)
