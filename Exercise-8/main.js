9757d4889450f9f9425cbb38acb77b20370e52d9
mohamed-awil <mohamedawil110@gmail.com>
2026-03-23 12:39:12 +0300

    exercise-8 submited

---
+++ Exercise-8/main.js
@@ -0,0 +1,11 @@
+let car = {
+    make: "Lamborghini",
+    model: "Lamborghini Revuelto",
+    year: 2026
+}
+
+console.log(car);
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
