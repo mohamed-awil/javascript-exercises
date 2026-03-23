09c061850d7bb5ba59c49d6e27f3954453ba0269
mohamed-awil <mohamedawil110@gmail.com>
2026-03-23 12:58:27 +0300

    exercise-9 submited

---
+++ Exercise-9/main.js
@@ -0,0 +1,12 @@
+// object methods
+
+let car = {
+    make: "Lamborghini",
+    model: "Lamborghini Revuelto",
+    year: 2026,
+    start: function () {
+        console.log("The car has started");
+    }
+}
+
+console.log(car.start());
