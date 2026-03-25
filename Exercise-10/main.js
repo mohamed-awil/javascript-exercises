3810f9b837a79f7df03ef87bbaa6647d2a63c2e4
mohamed-awil <mohamedawil110@gmail.com>
2026-03-25 20:15:19 +0300

    exercise-10 submited

---
+++ Exercise-10/main.js
@@ -0,0 +1,21 @@
+// arrays objects
+
+let library = [
+    {
+        title: "Eloquent JavaScript",
+        author: "Marijn Haverbeke",
+        year: 2018,
+    },
+
+    {
+        title: "HTML and CSS: Design and Build Websites",
+        author: "Jon Duckett",
+        year: 2011,
+    }
+]
+
+//  the title of the first book
+console.log(library[0].title);
+
+//  the author of the second book
+console.log(library[1].author);
