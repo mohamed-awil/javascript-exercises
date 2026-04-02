214eac99d528314460efb1924a80d6bd2aa691f5
mohamed-awil <mohamedawil110@gmail.com>
2026-04-02 16:04:37 +0300

    exercise-17 submited

---
+++ Exercise-17/main.js
@@ -0,0 +1,13 @@
+// else-if statement
+
+let temperature = 0;
+
+if (temperature < 0) {
+  console.log("Very cold");
+} else if (temperature <= 15) {
+  console.log("Cold");
+} else if (temperature <= 25) {
+  console.log("Warm");
+} else {
+  console.log("Hot");
+}
