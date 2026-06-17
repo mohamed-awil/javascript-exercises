f1b6da4e817638b9794d08076aa35ba3ae54c976
mohamed-awil <mohamedawil110@gmail.com>
2026-06-17 11:08:51 +0300

    exercise-32 submited

---
+++ Exercise-32/main.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("h1")
+console.log(header)
+
+const name = document.querySelector("#name")
+console.log(name)
+
+const text = document.querySelectorAll(".text")
+console.log(text)
